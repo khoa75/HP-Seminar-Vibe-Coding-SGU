3e64418e28a9d0df5e07faa97b72a41b62e47508
danhdct122c3 <144209033+danhdct122c3@users.noreply.github.com>
2026-01-31 00:29:55 +0700

    #6 - danh lam xong phan 3 - migrate java from python

--- Tuan01/copilot-workspace/java/socialapp/src/main/java/com/contoso/socialapp/SocialappApplication.java
+++ Tuan01/copilot-workspace/java/socialapp/src/main/java/com/contoso/socialapp/SocialappApplication.java
@@ -2,8 +2,10 @@ package com.contoso.socialapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
 public class SocialappApplication {
 
 	public static void main(String[] args) {
